7be2b5caf87d67ba07f248d0f7bd4ed04f50e8a7
zetosbuilder <zetoshare@outlook.com>
2023-04-05 01:41:50 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # zetos-contract
-Contract source code
+
+### About Our Project
+
+Zetos Share (ZES) will be developed based on BNB Chain in the early stage and will develop cross-chain algorithm stable currency in the later stages, and gradually expand to other chains and platforms such as APTOS.
+
+Now deployed ZES,ZEC,ZEB Token to BSC Mainnet,
+And our website is ready, view [https://zetos.finance](https://zetos.finance) for more.
+
+#### ZES contract address
+
+0xbFba2bA43d7fdF19c251ff8bf9bE9DF4B7BC7218
+
+[view it on bscscan](https://bscscan.com/address/0xbfba2ba43d7fdf19c251ff8bf9be9df4b7bc7218)
+
+#### ZEC contract address
+
+0xEf9b190f8d207711608EE6E24d07C815b21966F3
+
+[view it on bscscan](https://bscscan.com/address/0xEf9b190f8d207711608EE6E24d07C815b21966F3)
+
+#### ZEB contract address
+
+0x3D731993DaF617974C8B868B5a89cA80C89dc6F6
+
+[view it on bscscan](https://bscscan.com/address/0x3D731993DaF617974C8B868B5a89cA80C89dc6F6)
+
+
+### TODO
+
+Mining pool coming soon
+Farms will be open soon
---
+++ contracts/ZebToken.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
+/// The Operator contract is used to provide external contracts (such as Treasury) with external calls for DeFi-related operations
+contract Operator is Context, Ownable {
+    address private _operator;
+
+    event OperatorTransferred(
+        address indexed previousOperator,
+        address indexed newOperator
+    );
+
+    constructor() {
+        _operator = _msgSender();
+        emit OperatorTransferred(address(0), _operator);
+    }
+
+    function operator() external view returns (address) {
+        return _operator;
+    }
+
+    modifier onlyOperator() {
+        require(
+            _operator == msg.sender,
+            'operator: caller is not the operator'
+        );
+        _;
+    }
+
+    function isOperator() external view returns (bool) {
+        return _msgSender() == _operator;
+    }
+
+    function transferOperator(address newOperator_) external onlyOwner {
+        _transferOperator(newOperator_);
+    }
+
+    function _transferOperator(address newOperator_) internal {
+        require(
+            newOperator_ != address(0),
+            'operator: zero address given for new operator'
+        );
+        emit OperatorTransferred(address(0), newOperator_);
+        _operator = newOperator_;
+    }
+}
+
+/// ZEB token
+contract ZebToken is ERC20Burnable, Operator {
+    constructor() ERC20('Zetos Bond', 'ZEB') {
+        _mint(msg.sender, 1 * 10**18);
+    }
+
+    function mint(address recipient_, uint256 amount_)
+        public
+        onlyOperator
+        returns (bool)
+    {
+        uint256 balanceBefore = balanceOf(recipient_);
+        _mint(recipient_, amount_);
+        uint256 balanceAfter = balanceOf(recipient_);
+        return balanceAfter >= balanceBefore;
+    }
+
+    function burn(uint256 amount) public override onlyOperator {
+        super.burn(amount);
+    }
+
+    function burnFrom(address account, uint256 amount)
+        public
+        override
+        onlyOperator
+    {
+        super.burnFrom(account, amount);
+    }
+}
